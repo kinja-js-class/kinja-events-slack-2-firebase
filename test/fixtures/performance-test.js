afa9bdf065e34fa617599acb2977775d10ca3abe
Oliver Kovacs <oliver@kinja.com>
2016-03-16 17:44:31 +0100

    Add test items to firebase

---
+++ test/fixtures/performance-test.js
@@ -0,0 +1,804 @@
+const performanceTestData = [
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "sem magna nec quam. Curabitur vel lectus. Cum sociis natoque penatibus et",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1489628688"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "massa non ante",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1481790549"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "dictum mi, ac mattis velit justo nec ante. Maecenas mi felis, adipiscing fringilla,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1439805011"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "neque. Morbi quis urna. Nunc quis arcu vel quam dignissim pharetra. Nam ac",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1453564626"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "nec urna et",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1483855965"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "vitae purus gravida sagittis. Duis gravida. Praesent eu",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1477249807"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1451248742"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "eget, dictum placerat, augue. Sed molestie. Sed id",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1430481741"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Proin non massa non ante bibendum ullamcorper. Duis cursus, diam",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1467979632"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "amet, consectetuer adipiscing",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1449446662"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "amet luctus vulputate, nisi sem semper",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1469011679"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "cursus purus. Nullam",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1474905901"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "ornare sagittis felis. Donec tempor, est ac mattis semper, dui lectus rutrum urna, nec",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1484984113"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "blandit mattis. Cras eget nisi dictum augue malesuada malesuada. Integer id",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1481177070"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "scelerisque, lorem ipsum sodales purus, in molestie tortor",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1454553885"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Maecenas malesuada fringilla est. Mauris eu turpis. Nulla aliquet. Proin velit. Sed malesuada augue ut",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1486163198"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Cras dolor dolor, tempus non, lacinia at,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1440071264"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "auctor ullamcorper, nisl arcu iaculis",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1447387660"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "neque sed dictum eleifend,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1438671263"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "nonummy ut, molestie in, tempus eu, ligula. Aenean euismod mauris eu elit. Nulla",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1447619970"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "ante. Vivamus non lorem vitae odio sagittis semper. Nam tempor diam dictum sapien. Aenean massa.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1433157713"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "lobortis risus. In",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1433970808"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "non enim. Mauris quis turpis vitae purus gravida sagittis. Duis gravida. Praesent",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1465467370"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "vitae, posuere at, velit. Cras lorem lorem, luctus ut, pellentesque",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1489263591"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Vivamus rhoncus. Donec est. Nunc ullamcorper, velit",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1467623460"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Donec tempus, lorem fringilla ornare placerat, orci lacus vestibulum lorem, sit amet ultricies sem",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1428789839"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Aliquam nec enim. Nunc ut erat. Sed nunc est, mollis non, cursus non, egestas",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1472611748"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "non enim commodo hendrerit. Donec porttitor tellus",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1466844118"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "lacus pede sagittis augue, eu tempor",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1448855956"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "lorem vitae odio sagittis semper. Nam tempor diam dictum sapien. Aenean",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1485521223"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "erat vel pede blandit congue. In scelerisque scelerisque dui. Suspendisse ac",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1487054726"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "nisi. Mauris nulla. Integer urna.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1441809585"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "mollis nec, cursus a, enim. Suspendisse",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1488632330"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Sed auctor odio a purus. Duis elementum, dui quis accumsan convallis, ante lectus convallis est,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1468041707"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "magna. Ut tincidunt orci quis lectus. Nullam suscipit,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1464250165"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "ut, pellentesque eget, dictum placerat, augue. Sed molestie. Sed id",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1454574286"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "lacus, varius et, euismod et, commodo at, libero. Morbi accumsan laoreet ipsum. Curabitur consequat, lectus",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1455573946"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "fermentum convallis ligula. Donec luctus aliquet odio.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1457925640"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "mattis ornare, lectus ante dictum mi, ac mattis velit justo nec ante. Maecenas",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1458469611"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Maecenas mi felis, adipiscing fringilla, porttitor vulputate, posuere vulputate, lacus. Cras interdum. Nunc sollicitudin",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1458163194"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "Integer id magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim tempor arcu. Vestibulum ut",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1463804204"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "magna et ipsum cursus vestibulum. Mauris magna. Duis dignissim",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1438693922"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "turpis non enim. Mauris quis turpis vitae purus gravida sagittis. Duis",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1471684270"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "ut lacus. Nulla tincidunt,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1433735986"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "pede. Nunc sed orci lobortis augue scelerisque mollis. Phasellus libero mauris, aliquam eu, accumsan sed,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1434359122"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Mauris ut quam vel sapien imperdiet ornare. In faucibus. Morbi vehicula. Pellentesque tincidunt tempus",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1429183181"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "vel, faucibus id, libero. Donec consectetuer mauris id",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1449332825"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eget magna. Suspendisse tristique",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1450087139"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "orci quis lectus. Nullam suscipit, est ac facilisis facilisis,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1431791825"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "aliquet odio. Etiam ligula tortor, dictum eu,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1483723908"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "consectetuer euismod est arcu ac orci. Ut semper pretium neque. Morbi quis",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1473357967"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "quam vel sapien imperdiet ornare. In faucibus. Morbi vehicula. Pellentesque",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1439530098"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "magna. Sed eu eros. Nam consequat dolor",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1484382006"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "elit. Etiam laoreet, libero et tristique pellentesque, tellus sem",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1469620986"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "diam lorem, auctor quis, tristique ac, eleifend vitae, erat. Vivamus nisi. Mauris nulla. Integer",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1472001390"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "augue scelerisque mollis. Phasellus libero mauris,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1453300038"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1477481109"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "auctor non, feugiat nec, diam. Duis mi enim, condimentum eget, volutpat ornare, facilisis eget, ipsum.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1461145396"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "lectus quis massa. Mauris vestibulum, neque sed dictum eleifend, nunc risus varius orci,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1438349446"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "neque venenatis lacus. Etiam bibendum fermentum metus. Aenean sed pede nec ante blandit",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1471928478"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "pretium neque. Morbi quis urna. Nunc quis arcu vel quam dignissim",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1473205592"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "primis in faucibus",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1474468755"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Nunc lectus pede, ultrices a, auctor",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1439069098"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "elementum, dui quis",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1427480213"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "dui quis accumsan convallis, ante lectus convallis est, vitae sodales",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1477306557"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "odio vel est tempor bibendum. Donec",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1471584326"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "mollis. Phasellus libero mauris, aliquam",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1487561263"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "eu nulla at sem molestie sodales. Mauris blandit enim consequat purus. Maecenas libero",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1458334390"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Cras convallis convallis dolor. Quisque tincidunt pede ac urna.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1458796918"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Cras sed leo.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1439489771"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "sit amet ante. Vivamus non lorem vitae odio sagittis semper.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1479923109"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Aliquam erat volutpat. Nulla facilisis. Suspendisse commodo tincidunt nibh.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1454677485"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "molestie orci tincidunt adipiscing. Mauris molestie pharetra nibh. Aliquam ornare, libero at auctor ullamcorper,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1439981491"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "semper egestas, urna",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1444914569"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "nec, euismod in, dolor. Fusce",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1488860357"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "convallis ligula. Donec luctus aliquet odio. Etiam",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1486792955"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "sociis natoque penatibus et magnis dis parturient montes, nascetur",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1453617299"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "orci lacus vestibulum lorem, sit amet",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1428325370"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "pede. Suspendisse dui. Fusce diam",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1445987046"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Quisque libero lacus, varius et, euismod et, commodo at, libero.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1448786441"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "semper, dui lectus rutrum urna, nec luctus felis purus ac",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1483705813"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "aliquet vel, vulputate eu, odio. Phasellus at augue id ante dictum cursus. Nunc mauris",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1434373988"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "nonummy ut, molestie in, tempus eu, ligula. Aenean euismod mauris eu",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1460403411"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "vitae dolor. Donec fringilla. Donec feugiat metus sit amet",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1438609371"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "sociosqu ad litora torquent per conubia",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1439474685"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "eu augue porttitor interdum. Sed auctor odio a purus. Duis elementum, dui",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1430871937"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "Proin velit. Sed malesuada augue ut lacus. Nulla tincidunt, neque vitae semper egestas,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1462766153"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "luctus sit amet, faucibus ut,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1467439142"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Donec tempor, est ac mattis semper,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1451144731"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "interdum feugiat. Sed nec metus facilisis",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1483441365"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "pede sagittis augue, eu tempor erat neque non quam. Pellentesque",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1437575199"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "ut ipsum ac mi",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1475183881"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "Nam ligula elit,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1452096152"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "facilisi. Sed neque. Sed eget lacus. Mauris non",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1483021422"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "egestas nunc sed libero. Proin sed",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1484505346"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "In mi pede,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1430548351"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "feugiat non, lobortis quis, pede. Suspendisse dui. Fusce diam nunc, ullamcorper eu, euismod ac,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1446414402"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "sapien. Aenean massa. Integer vitae nibh.",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1469865745"
+	},
+	{
+		"completed": true,
+		"severity": "info",
+		"text": "auctor vitae, aliquet nec, imperdiet nec, leo. Morbi neque tellus, imperdiet",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1436263294"
+	},
+	{
+		"completed": false,
+		"severity": "info",
+		"text": "luctus aliquet odio. Etiam ligula tortor, dictum eu, placerat eget, venenatis a,",
+		"type": "slack",
+		"meta": {},
+		"timestamp": "1441570227"
+	}
+]
+
+export default performanceTestData
